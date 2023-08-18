d007fadaa5fe61e341a85fde7784212c84b4fe14
thenareshofficial <thenareshoffcialgmail.com>
2023-08-19 02:36:00 +0530

    heder.php file added

--- header.php
+++ header.php
@@ -1,1 +1,21 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Header</title>
+</head>
+<body>
+        <div class="container">
+                <div class="right">
+                        <a href="#">The Naresh Offcial</a>
+                </div>
+                <div class="left">
+                        <a href="#">Home</a>
+                        <a href="#">Skill</a>
+                        <a href="#">Project</a>
+                        <a href="#">Contact</a>
+                </div>
+        </div>
+</body>
+</html>
