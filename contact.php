9f37c48a6f739c6b997f252b21c8f29099b4b19a
thenareshofficial <thenareshoffcialgmail.com>
2023-08-19 19:00:32 +0530

    contact.php file added

---
+++ contact.php
@@ -0,0 +1,17 @@
+
+<?php include "header.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/contact.css">
+        <title>Contact</title>
+</head>
+<body>
+        <h1 align='center'>This is <u>Contact</u> Page</h1>
+</body>
+</html>
+
+<?php include "footwear.php" ?>
